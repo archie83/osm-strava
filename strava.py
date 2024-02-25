77c72a61f977cbb8668044e894b1d8342ce94978
Jean-Claude Repetto <git@repetto.org>
2024-02-25 09:26:41 +0100

    Set user agent to Firefox

--- strava.py
+++ strava.py
@@ -160,7 +160,7 @@ def fetch_strava_tile(zoom, x, y):
     url = f'https://strava-heatmap.tiles.freemap.sk/{activity}/hot/{zoom}/{x}/{y}.png'
     print_verbose("Downloading Strava tile at", url)
     try:
-        r = requests.get(url, allow_redirects=True, headers={'user-agent': 'curl/8.5.0'})
+        r = requests.get(url, allow_redirects=True, headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:123.0) Gecko/20100101 Firefox/123.0'})
         r.raise_for_status()
     except requests.exceptions.HTTPError as e:
         print_debug("Status code =", e.response.status_code)
